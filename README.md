5ecb7a930c59b750642eb6c43358de0a8d4bf40b
HannahStarcevich <starcevichh@gmail.com>
2020-12-10 19:33:10 -0800

    final updates to project

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # JavaScript-Password-Generator_HS-H3
 This application is designed to generate a random password for employees.
+
+This site: https://hannahstarcevich.github.io/JavaScript-Password-Generator_HS-H3/
+My GitHub: https://github.com/HannahStarcevich/JavaScript-Password-Generator_HS-H3
+
+The goal of this project was to develop a simple website interface and easy to use password generator. The password generated includes a completely random selection of uppercase and lowercase letters, numbers and special characters. 
+
+Elements used in this project: JavaScript functions, for loops, events, methods arrays and more. 
+
+This website demonstrates my first fully developed and launched JavaScript code. 
+
+Contact info: Email: Starcevichh@gmail.com LinkedIn: https://www.linkedin.com/in/hannah-starcevich/ GitHub: https://github.com/HannahStarcevich
--- script.js
+++ script.js
@@ -1,106 +1,51 @@
-// Assignment Code
+// connecting id on the html with the js file by assigning variable.
 var generateBtn = document.querySelector("#generate");
-var randomIndex1 = 0
 
-// Write password to the #password input
-function thisIsTheFirstFunctionWhenButtonIsClicked() {
-  var password = generateRandomAssPassword();
-  // document.getElementById("password").innerHTML = fruits.join(" * ");
+// set up the button click event to trigger the generate password function.
+function buttonClick() {
 
+  var password = generateRandomPassword();
+  // display the password generated on the site. 
   var passwordText = document.querySelector("#password");
+  // join the password characters together in a string.
   passwordText.value = password.join("");
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", thisIsTheFirstFunctionWhenButtonIsClicked);
+// event listener to click and generate password.
+generateBtn.addEventListener("click", buttonClick);
 
-function generateRandomAssPassword() {
+// generate the password.
+function generateRandomPassword() {
 
   var passwordLength = prompt("Identify the length of your password by submitting a number between 8 - 128.");
   console.log(passwordLength);
+  // transform the number submitted by the user into an integer. 
   var passwordLength = parseInt(passwordLength);
   console.log(passwordLength);
 
+  // validate user input of password length.
   if (passwordLength < 8 || passwordLength > 128) {
-    // validate user input of password length
     alert("This input is invalid. Please input another number in between 8 - 128.");
     return;
   }
 
-  var myDopePool = buildMyPoolOfCharacters();
-  console.log(myDopePool);
-  var myDopePassword = [];
+  // trigger the function to build the pool of characters and use it's output.
+  var myCompletePool = buildMyPoolOfCharacters();
+  console.log(myCompletePool);
+  // aggregate all the selected characters in the password array.
+  var myAmazingPassword = [];
+  // store random number generated to pull a character from myPool.
+  var randomIndex1 = 0
 
+  // generate a random number to select the index from the complete pool and add it to the password array for the length of the password selected by the user.
   for (var i = 0; i < passwordLength; i++) {
-
-    var randomIndex1 = Math.floor(Math.random() * myDopePool.length);
-    // console.log(myDopePool[randomIndex1]);
-    myDopePassword.push(myDopePool[randomIndex1]);
-
+    var randomIndex1 = Math.floor(Math.random() * myCompletePool.length);
+    myAmazingPassword.push(myCompletePool[randomIndex1]);
   }
-  return myDopePassword;
+  return myAmazingPassword;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// build a pool of the types of characters selected by the user. for 'true' user submissions, the array is consolidated (concat) and added to the pool.
 function buildMyPoolOfCharacters() {
   var myPool = [];
   var passwordLower = confirm("Would you like lowercase characters in your password?");
@@ -122,11 +67,10 @@ function buildMyPoolOfCharacters() {
   }
 
   var passwordSpecChar = confirm("Would you like special characters characters in your password for extra security?");
-  var array4 = ("!", "#", "$", "%", "&", "*");
+  var array4 = ["!", "#", "$", "%", "&", "*", "(", ")", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "~"];
   if (passwordSpecChar) {
     myPool = myPool.concat(array4);
   }
 
   return myPool;
-
 }
